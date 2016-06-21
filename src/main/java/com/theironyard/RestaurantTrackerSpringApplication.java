94d92362be4a340e0c8c4e114c1f497d188d8729
willriggins <williamriggins2013@gmail.com>
2016-06-21 10:14:42 -0400

    initial import

---
+++ src/main/java/com/theironyard/RestaurantTrackerSpringApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantTrackerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantTrackerSpringApplication.class, args);
+	}
+}
